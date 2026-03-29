fa874b36b5b4a0b5b7bca5c062c7518078e72b03
lucascogrossi <contact@lucascogrossi.com>
2026-03-29 16:15:32 -0300

    add gauss_seidel_rb_kernel

--- cuda/smoothers.cuh
+++ cuda/smoothers.cuh
@@ -20,4 +20,24 @@ __global__ void jacobi_kernel(Grid2D* grid, double* u_new) {
     }
 }
 
+// 0: red | 1: black
+__global__ void gauss_seidel_rb_kernel(Grid2D* grid, int color) {
+
+    double hx2 = grid->hx * grid->hx;
+    double hy2 = grid->hy * grid->hy;
+    double diag = 2.0 * (1.0/hx2 + 1.0/hy2);
+
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i >= 1 && i < grid->nx && j >= 1 && j < grid->ny) {
+        // Divergencia de threads (otimizar dps)
+        if ((i + j) % 2 == color) {
+                grid->u[grid->idx(i, j)] = ((grid->u[grid->idx(i-1, j)] + grid->u[grid->idx(i+1, j)]) / hx2 +
+                                          (grid->u[grid->idx(i, j-1)] + grid->u[grid->idx(i, j+1)]) / hy2 +
+                                          grid->f[grid->idx(i,j)]) / diag;
+        }
+    }
+}
+
 #endif
